ae2c75cac5feff8e5bbbabe70910f972d956dc47
Konstantine Arkoudas <ka.ghcode@gmail.com>
2024-09-04 09:55:34 -0400

    Subproof extraction working

--- ft/data/book/process.py
+++ ft/data/book/process.py
@@ -51,7 +51,6 @@ def updateParenState(paren_state,c):
     else:
         pass
 
-
 def checkBalance(text):
     paren_state = {'parens':0, 'brackets':0, 'braces':0}
     for c in text:
@@ -81,9 +80,15 @@ def balanceText(text,paren_state):
         print("UNABLE TO BALANCE REMAINDER!")
         raise Exception("UNABLE TO BALANCE REMAINDER!")
 
-def getText(lines,start,end):
-    (line1,pos1) = start
-    (line2,pos2) = end
+def getBalancedText(lines,start_pos,end_pos):
+    '''
+    lines:A string of lines representing the entire literal contents of the original source file for the proof of interest 
+    start_pos: A pair of integers (line,pos), 0-indexed, representing the starting position (line and column number) for the proof or subproof of interest 
+    end_pos: A pair of integers (line,pos), 0-indexed, representing the ending position (line and column number) for the proof or subproof of interest 
+    Output: The text of the unique proof that starts at start_pos and ends either at end_pos or beyond. 
+    '''
+    (line1,pos1) = start_pos
+    (line2,pos2) = end_pos
     subtract = len(lines[line2])-pos2
     span_text = ''.join(lines[line1:line2+1])[pos1:]
     span_text_clipped = span_text[:-subtract]
@@ -96,21 +101,76 @@ def getText(lines,start,end):
     else:
         return span_text_clipped + balanceText(all_remaining_text,paren_state)
 
+
+def getSingleFidContent(single_fid_text):
+    #print("\nWorking on this single fid entry: " + single_fid_text)
+    s = single_fid_text
+    chunks = [chunk.strip() for chunk in s.split(' || ')]
+    # By convention, we must have exactly three ||-separated chunks: 
+    assert len(chunks) == 3
+    fid_name_literal, fid_type_literal, fid_value_literal = 'fidName:', 'fidType:', 'fidValue:'
+    # Get name first: 
+    fid_name = chunks[0][len(fid_name_literal):].strip()
+    # Then get type: 
+    fid_type = chunks[1][len(fid_type_literal):].strip()
+    # And finally value: 
+    fid_value = chunks[2][len(fid_value_literal):].strip()
+    return {'fid_name': fid_name, 'fid_type': fid_type, 'fid_value': fid_value}
+
+def getFidBlockInfo(fid_content):
+    R = []
+    i = 0
+    while (fid_content.find("fidName: ",i)) >= 0: 
+        i = fid_content.find("fidName: ",i)
+        next = fid_content.find("fidName: ",i + 1 + len('fidName: '))
+        single_fid_text = fid_content[i:] if next < 0 else fid_content[i:next].strip()
+        R.append(getSingleFidContent(single_fid_text))
+        i = next 
+    return R
+
+def getRange(text): 
+    line_index_1 = findNthOccurrence(text,':',2)
+    pos_index_1 = text.find(':',line_index_1+1)
+    starting_line = int(text[line_index_1+1:pos_index_1].strip())
+    pos_1_end = text.find("and ending at: ") 
+    if pos_1_end < 0:
+        pos_1_end = len(text)
+    starting_pos = int(text[pos_index_1+1:pos_1_end].strip())
+    return (starting_line,starting_pos)
+
+def subproofStartingAndEndingPositions(subproof_chunk): 
+    (starting_line,starting_pos) = getRange(subproof_chunk)
+    pat = " and ending at: "
+    pos_1_end = subproof_chunk.find(pat)
+    rem_text = subproof_chunk[pos_1_end+4:subproof_chunk.find('\n')]
+    (ending_line,ending_pos) = getRange(rem_text)
+    return ((starting_line-1,starting_pos-1), (ending_line-1,ending_pos-1))
+
+def getSubproofs(subproof_content,source_file,all_source_file_lines): 
+    chunks = [chunk.strip() for chunk in subproof_content.split("||\n")]
+    R = []
+    for chunk in chunks:
+        (subproof_starting_pos,subproof_ending_pos) = subproofStartingAndEndingPositions(chunk)
+        subproof_text = getBalancedText(all_source_file_lines,subproof_starting_pos,subproof_ending_pos)
+        R.append({'subproof_text': subproof_text, 'file': source_file, 'starting_pos': {'line': subproof_starting_pos[0], 'col': subproof_starting_pos[1]}, 'ending_pos': {'line': subproof_ending_pos[0], 'col': subproof_ending_pos[1]}})
+    return R 
+
 def analyzeBlock(block):
     '''
     This function takes a proof block and extracts the proof and the proof's metadata from it. The input block is just a list of lines,
     and the output is a dictionary of this form:
-    {'proof': <the text of the proof>,
-     'file':  <the file of the proof>,
-     'start_pos': <the starting position>,
-     'end_pos':   <the ending position>,
-     'conclusion': <the conclusion of the proof, pretty-formatted>,
-     'free-ids':   <a dictionary, mapping each free identifier in the proof to its value> 
-     'fun-syms':   <a dictionary mapping function symbols that appear in the proof to their signatures>,
-     'structures': <a list of all the relevant structure definitions. Each structure contains its file, start and end pos, and the text of the definition>,
-     'domains':    <a list of all non-structure domains that appear in the signatures of 'fun-syms'
+    {'proof_text':  <the text of the proof>,
+     'file':        <the file of the proof>,
+     'start_pos':   <the starting position>,
+     'end_pos':     <the ending position>,
+     'conclusion':  <the conclusion of the proof, pretty-formatted>,
+     'subproofs':   <a list of all the subdeductions, each represented as a dictionary with at least one field: 'subproof_text'>
+     'free-ids':    <a list of free identifier records, each of the form {'fid_name': ..., 'fid_type': ..., 'fid_value': ...}> 
+     'fun-syms':    <a dictionary mapping function symbols that appear in the proof to their signatures>,
+     'structures':  <a list of all the relevant structure definitions. Each structure contains its file, start and end pos, and the text of the definition>,
+     'domains':     <a list of all non-structure domains that appear in the signatures of 'fun-syms'
     ''' 
-    text = '\n'.join(block)
+    text = ''.join(block)
     header_line = block[1]
     assert header_line.startswith('qqq')
     file_start_pos = header_line.find('position: ') + 10
@@ -121,21 +181,48 @@ def analyzeBlock(block):
     diff = column_start_pos - line_start_pos - 1 
     line_number = header_line[line_start_pos:line_start_pos+diff]
     col_number = header_line[column_start_pos:].strip()
-    proof_starting_position = {'file':file_name, 'line': int(line_number)-1, 'pos': int(col_number)-1}
+    proof_starting_position = {'line': int(line_number)-1, 'pos': int(col_number)-1}
     second_line = block[2]
     i = findNthOccurrence(second_line,':',3)
     proof_end_line = int(second_line[1+findNthOccurrence(second_line,':',2):i])-1
     proof_end_pos = int(second_line[i+1:].strip())-1
-    L = getTotallyLiteralLines(file_name)
-    proof_text = getText(L,(proof_starting_position['line'],proof_starting_position['pos']),(proof_end_line,proof_end_pos))
-    print('\n=== PROOF TEXT:\n' + proof_text)
+    all_source_file_lines = getTotallyLiteralLines(file_name)
+    proof_text = getBalancedText(all_source_file_lines,(proof_starting_position['line'],proof_starting_position['pos']),(proof_end_line,proof_end_pos))    
+    lines_in_proof_text = proof_text.count('\n')
+    last_eol_pos = proof_text.rstrip().rfind('\n')
+    last_eol_pos = 0 if last_eol_pos < 0 else last_eol_pos
+    last_chunk = proof_text[1+last_eol_pos:].rstrip()
+    proof_ending_position = {'line': proof_starting_position['line']+lines_in_proof_text-1,'pos': len(last_chunk)-1}
+    # So far: proof_text, file_name,proof_starting_position, proof_ending_position 
+    conclusion_pat = 'Conclusion:\n{{{'
+    conclusion_pos = text.find(conclusion_pat)
+    conclusion_start = conclusion_pos + len(conclusion_pat)
+    conclusion_end = text.find('}}}',conclusion_start)
+    conclusion_text = text[conclusion_start:conclusion_end].strip()
+    #**** Free ids:
+    fid_pat = 'Free id values and types:\n{{{'
+    fid_start_pos = text.find(fid_pat)
+    fid_end_pos = text.find("}}}",fid_start_pos+len(fid_pat))
+    fid_content = text[fid_start_pos+len(fid_pat):fid_end_pos].strip()
+    fid_list = getFidBlockInfo(fid_content)
+    #**** Subproofs: 
+    subproof_pat = "All subdeductions:\n{{{"
+    subproofs_start = text.find(subproof_pat)
+    subproofs_end = text.find('}}}',subproofs_start+len(subproof_pat))
+    subproof_content = text[subproofs_start+len(subproof_pat):subproofs_end].strip()
+    subproof_list = sorted(getSubproofs(subproof_content,file_name,all_source_file_lines),key=lambda subproof: len(subproof['subproof_text']))
+
+
 
 def processFile(file):
-    L = getLiteralLines(file) 
+    L = getTotallyLiteralLines(file) 
     blocks = getProofBlocks(L)
     return ''
 
 file = 'chapter03.txt'
 
-for b in blocks:
-    analyzeBlock(b)
+
+#L = getTotallyLiteralLines(file) 
+#blocks = getProofBlocks(L)
+#b = blocks[32]
+#block = b
